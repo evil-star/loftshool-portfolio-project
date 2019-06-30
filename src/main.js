2d91b576b0d2d870fd174be567135162fbc01f90
Igor <igor.bakin.v@gmail.com>
2019-06-30 13:49:53 +0300

    Initial commit

--- src/main.js
+++ src/main.js
@@ -3,4 +3,15 @@ if (process.env.NODE_ENV === "development") {
   require("file-loader!./index.pug");
 }
 
+const menuToggleEl = document.querySelectorAll(
+  ".menu-btn, .menu-mobile__close, .menu-mobile__list"
+);
+const menuMobile = document.querySelector(".menu-mobile");
+
+menuToggleEl.forEach(function(el) {
+  el.addEventListener("click", function() {
+    menuMobile.classList.toggle("menu-mobile_active");
+  });
+});
+
 import "./scripts/skills";
