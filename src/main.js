ef3543f79f5c2d86e2ee2222a059cbf911d5b015
Igor <igor.bakin.v@gmail.com>
2019-07-06 11:42:58 +0300

    начата реализация параллакса

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ if (process.env.NODE_ENV === "development") {
   require("file-loader!./index.pug");
 }
 
+let parallaxScene = document.querySelector(".parallax");
+let parallaxLayers = parallaxScene.querySelectorAll(".parallax__layer");
+let scrollTop;
+window.onscroll = function() {
+  scrollTop = window.scrollY;
+  console.log(scrollTop);
+};
+
 const menuToggleEl = document.querySelectorAll(
   ".menu-btn, .menu-mobile__close, .menu-mobile__list"
 );
