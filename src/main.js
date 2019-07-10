14d7feaed4c8d37d7bfb44a087a9e906a70869a5
Igor <igor.bakin.v@gmail.com>
2019-07-10 16:02:39 +0300

    Отключен параллакс для мобильных устройств

--- src/main.js
+++ src/main.js
@@ -3,17 +3,19 @@ if (process.env.NODE_ENV === "development") {
   require("file-loader!./index.pug");
 }
 
-let parallaxScene = document.querySelector(".parallax");
-let parallaxLayers = parallaxScene.querySelectorAll(".parallax__layer");
-let scrollTop;
-let speed;
-window.onscroll = function() {
-  scrollTop = window.scrollY;
-  parallaxLayers.forEach(el => {
-    speed = scrollTop * el.dataset.speed;
-    el.style.transform = `translateY(${speed}px) translateZ(0)`;
-  });
-};
+if (window.matchMedia("(min-width: 768px)").matches) {
+  let parallaxScene = document.querySelector(".parallax");
+  let parallaxLayers = parallaxScene.querySelectorAll(".parallax__layer");
+  let scrollTop;
+  let speed;
+  window.onscroll = function() {
+    scrollTop = window.scrollY;
+    parallaxLayers.forEach(el => {
+      speed = scrollTop * el.dataset.speed;
+      el.style.transform = `translateY(${speed}px) translateZ(0)`;
+    });
+  };
+}
 
 const menuToggleEl = document.querySelectorAll(
   ".menu-btn, .menu-mobile__close, .menu-mobile__list"
